191d8d59d751c057f467570098c58dc775ccb00d
StrayDogSyndications <StrayDogSyndicationsLLC@gmail.com>
2025-08-30 09:44:47 -0400

    Add initial implementation of Titanic passenger data using Pandas

---
+++ Onboarding/pandas.py
@@ -0,0 +1,17 @@
+# AISE 2026
+# Let’s get started with Python and Pandas!
+
+import pandas as pd
+
+# Store passenger data of the Titanic
+df = pd.DataFrame({
+   "Name": [
+      "Braund, Mr. Owen Harris",
+      "Allen, Mr. William Henry",
+      "Bonnell, Miss. Elizabeth",
+   ],
+   "Age": [22, 35, 58],
+   "Sex": ["male", "male", "female"],
+})
+
+print(df)
